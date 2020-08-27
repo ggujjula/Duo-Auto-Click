aba3514cca38a64cc099f57dea35de052e17bb32
Gautham Gujjula <gauthamgujjula@utexas.edu>
2020-08-26 20:07:00 -0500

    Added a MutationObserver to detect the load of the row with the targeted button. This does not work yet, likely due to cross-domain restrictions.

--- dac.js
+++ dac.js
@@ -1,3 +1,17 @@
 console.log("Duo Auto-Push injected");
 
+var duoIframeClass = "duo_iframe";
+var targetButton = "push-label";
+var iframeNode = document.getElementById(duoIframeClass);
+var observerConfig = { attributes: false, childList: true, subtree: true };
 
+var iframeLoadWaiter = new MutationObserver(function(mutations){
+  console.log("Called back");
+  var targetButtonNode = iframeNode.getElementsByClassName(targetButton);
+  if(targetButtonNode.length != 0){
+    console.log("target row loaded");
+    iframeLoadWaiter.disconnect();
+  }
+});
+
+iframeLoadWaiter.observe(iframeNode, observerConfig);
